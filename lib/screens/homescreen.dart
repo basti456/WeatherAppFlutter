2c6ce9d2951a4a1600dcefcded82b2054effa0be
basti456 <ekagraagrawal49@gmail.com>
2024-06-21 18:32:43 +0530

    wip:homescreen

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:weather_app/components/weather_item.dart';
 import 'package:weather_app/widgets/constants.dart';
+import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:ui';
+import 'package:flutter/services.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -26,9 +32,52 @@ class _HomeScreenState extends State<HomeScreen> {
   List dailyWeatherForecast = [];
 
   String currentWeatherStatus = '';
-
   String searchWeatherAPI =
       "http://api.weatherapi.com/v1/current.json?key=$API_KEY&days=7&q=";
+
+  void fetchWeatherData(String searchText) async {
+    try {
+      var searchResult =
+          await http.get(Uri.parse(searchWeatherAPI + searchText));
+      final weatherData = Map<String, dynamic>.from(
+          json.decode(searchResult.body) ?? 'No Data');
+      var locationData = weatherData['location'];
+      var currentWeather = weatherData['current'];
+
+      setState(() {
+        location = getShortLocationName(locationData['name']);
+        var parsedDate =
+            DateTime.parse(locationData["localtime"].substring(0, 10));
+        var newDate = DateFormat('MMMMEEEd').format(parsedDate);
+        currentDate = newDate;
+        currentWeatherStatus = currentWeather['condition']['text'];
+        weatherIcon =
+            "${currentWeatherStatus.replaceAll(' ', '').toLowerCase()}.png";
+        temperature = currentWeather["temp_c"].toInt();
+        humidity = currentWeather['humidity'].toInt();
+        windspeed = currentWeather['wind_kph'].toInt();
+        cloud = currentWeather['cloud'].toInt();
+
+        dailyWeatherForecast = weatherData['forecast']['forecastday'];
+        hourlyWeatherForecast = dailyWeatherForecast[0]['hour'];
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  static String getShortLocationName(String s) {
+    List<String> wordList = s.split(' ');
+    if (wordList.isNotEmpty) {
+      if (wordList.length > 1) {
+        return '${wordList[0]} ${wordList[1]}';
+      } else {
+        return wordList[0];
+      }
+    } else {
+      return "";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,19 +116,290 @@ class _HomeScreenState extends State<HomeScreen> {
                     children: [
                       Image.asset(
                         'assets/menu.png',
-                        width: 20,
+                        width: 35,
+                        height: 35,
                       ),
-                      const SizedBox(
-                        width: 3,
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/pin.png',
+                            width: 20,
+                          ),
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          Text(
+                            location,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              _cityController.clear();
+                              showBarModalBottomSheet(
+                                context: context,
+                                builder: (context) => SingleChildScrollView(
+                                  controller: ModalScrollController.of(context),
+                                  child: Container(
+                                    height: size.height * .2,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 20, vertical: 10),
+                                    child: Column(
+                                      children: [
+                                        SizedBox(
+                                          width: 70,
+                                          child: Divider(
+                                            thickness: 3.5,
+                                            color: _constants.primaryColor,
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        TextField(
+                                          onChanged: (searchText) {
+                                            fetchWeatherData(searchText);
+                                          },
+                                          controller: _cityController,
+                                          autofocus: true,
+                                          decoration: InputDecoration(
+                                            prefixIcon: Icon(
+                                              Icons.search,
+                                              color: _constants.primaryColor,
+                                            ),
+                                            suffixIcon: GestureDetector(
+                                              onTap: () {
+                                                _cityController.clear();
+                                              },
+                                              child: Icon(
+                                                Icons.close,
+                                                color: _constants.primaryColor,
+                                              ),
+                                            ),
+                                            hintText: 'Search city e.g. London',
+                                            focusedBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                color: _constants.primaryColor,
+                                              ),
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                            icon:
+                                const Icon(Icons.keyboard_arrow_down_outlined),
+                            color: Colors.white,
+                            iconSize: 20,
+                          ),
+                        ],
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: const Icon(
+                          Icons.person_rounded,
+                          size: 35,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 160,
+                    child: Image.asset("assets/$weatherIcon"),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(
+                          temperature.toString(),
+                          style: TextStyle(
+                              fontSize: 80,
+                              fontWeight: FontWeight.bold,
+                              foreground: Paint()..shader = _constants.shader),
+                        ),
                       ),
                       Text(
-                        location,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      )
+                        'o',
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            foreground: Paint()..shader = _constants.shader),
+                      ),
                     ],
+                  ),
+                  Text(
+                    currentWeatherStatus,
+                    style: const TextStyle(
+                      color: Colors.white70,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    currentDate,
+                    style: const TextStyle(
+                      color: Colors.white70,
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: const Divider(
+                      color: Colors.white70,
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        WeatherItem(
+                            value: windspeed.toInt(),
+                            unit: 'km/h',
+                            imgUrl: 'assets/windspeed.png'),
+                        WeatherItem(
+                            value: humidity.toInt(),
+                            unit: 'km/h',
+                            imgUrl: 'assets/humidity.png'),
+                        WeatherItem(
+                            value: cloud.toInt(),
+                            unit: 'km/h',
+                            imgUrl: 'assets/cloud.png'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 10),
+              height: size.height * .2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Today',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          'Forecasts',
+                          style: TextStyle(
+                              fontSize: 16, color: _constants.primaryColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  SizedBox(
+                    height: 100,
+                    child: ListView.builder(
+                      itemCount: hourlyWeatherForecast.length,
+                      scrollDirection: Axis.horizontal,
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+                        String currentTime =
+                            DateFormat('HH:mm:ss').format(DateTime.now());
+                        String currentHour = currentTime.substring(0, 2);
+                        String forecastTime = hourlyWeatherForecast[index]
+                                ['time']
+                            .substring(11, 16);
+                        String forecastHour = hourlyWeatherForecast[index]
+                                ['time']
+                            .substring(11, 113);
+                        String forecastWeatherName =
+                            hourlyWeatherForecast[index]["condition"]["text"];
+                        String forecastWeatherIcon =
+                            "${forecastWeatherName.replaceAll(' ', '').toLowerCase()}.png";
+                        String forecastTemperature =
+                            hourlyWeatherForecast[index]["temp_c"]
+                                .round()
+                                .toString();
+                        return Container(
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          margin: const EdgeInsets.only(right: 20),
+                          width: 60,
+                          decoration: BoxDecoration(
+                            color: currentHour == forecastHour
+                                ? _constants.primaryColor
+                                : Colors.white,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(50),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: const Offset(0, 1),
+                                blurRadius: 5,
+                                color: _constants.primaryColor.withOpacity(.2),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                forecastTime,
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  color: _constants.greyColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Image.asset(
+                                'assets/$forecastWeatherIcon',
+                                width: 20,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    forecastTemperature,
+                                    style: TextStyle(
+                                      fontSize: 17,
+                                      color: _constants.greyColor,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    'o',
+                                    style: TextStyle(
+                                      fontSize: 17,
+                                      color: _constants.greyColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontFeatures: const [
+                                        FontFeature.enable('sups')
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   )
                 ],
               ),
